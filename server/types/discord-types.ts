d236f222d88df38c184fe964dd16f63cfccbd885
seantokuzo <seantokuzo@gmail.com>
2023-11-21 12:43:42 -0800

    add discord response types

---
+++ server/types/discord-types.ts
@@ -0,0 +1,23 @@
+export enum DISCORD_API_ROUTES {
+  GET_TOKEN = 'https://discord.com/api/v10/oauth2/token',
+  GET_USER = 'https://discord.com/api/v10/users/@me',
+  GET_SERVERS = 'https://discord.com/api/v10/users/@me/guilds',
+  REVOKE_TOKEN = 'https://discord.com/api/v10/oauth2/token/revoke',
+}
+
+export interface DiscordUserResponse {
+  id: string
+  username: string
+  avatar: string | null
+}
+
+export interface DiscordServer {
+  id: string
+  name: string
+  icon: string
+}
+
+export enum ACCEPTED_SERVERS {
+  ECRI44 = '1165997893824561213',
+  CRTI19ECRI43ECRI44 = '1172662083750473748',
+}
