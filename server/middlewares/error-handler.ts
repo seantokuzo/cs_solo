a51466aced5b880e068ef1af0583d8fc2e224f28
seantokuzo <seantokuzo@gmail.com>
2023-11-21 12:42:44 -0800

    update error log

--- server/middlewares/error-handler.ts
+++ server/middlewares/error-handler.ts
@@ -10,7 +10,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send(err.serializeErrors())
   }
-  console.error(err)
+  console.error('❌ Not Custom Error', err)
   res.status(500).send({
     errors: [{ message: 'Something went wrong' }],
   })
