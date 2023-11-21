0efb177ecbb04c75a655c3b840eecf0223840402
seantokuzo <seantokuzo@gmail.com>
2023-11-21 12:47:05 -0800

    add user router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import authRouter from './auth-router'
+import userRouter from './user-router'
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
+router.use('/me', userRouter)
 
 export default router
