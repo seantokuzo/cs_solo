6a2157f99d8e7d0312c20d578ba4b2a5fcc40950
seantokuzo <seantokuzo@gmail.com>
2023-11-20 16:30:13 -0800

    initial commit -project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full max-w-[100vw] min-h-screen flex flex-col justify-center items-center">
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        colorLight: "#F4F2EC",
+        colorDark: "#0d0d0d",
+        discord: "#5965F2",
+        gold: "#fbbf24",
+        silver: "#94a3b8",
+        bronze: "#b45309",
+        buttonGo: "#60a5fa",
+        danger: "#f87171",
+        success: "#22c55e",
+      },
+      boxShadow: {
+        basic:
+          "2px 2px 6px rgba(0, 0, 0, 0.35), 3px 3px 6px rgba(0, 0, 0, 0.25)",
+        basicLg:
+          "6px 6px 10px rgba(0, 0, 0, 0.35), 8px 8px 10px rgba(0, 0, 0, 0.25)",
+        shDiscord: "inset 6px 6px 12px #3d46a7, inset -6px -6px 12px #7584ff",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-4turn)" },
+        },
+        revspin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(4turn)" },
+        },
+        fadein: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        loadspin: "spin 5s linear infinite",
+        revspin: "revspin 5s linear infinite",
+        fadein: "fadein 1000ms ease-out forwards",
+      },
+    },
+  },
+  plugins: [],
+}
