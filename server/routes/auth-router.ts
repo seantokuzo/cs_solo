ae8452f91eec1300b65cd2969cd31937aeea9e9d
seantokuzo <seantokuzo@gmail.com>
2023-11-21 12:47:29 -0800

    fix signin method

--- server/routes/auth-router.ts
+++ server/routes/auth-router.ts
@@ -7,7 +7,7 @@ import {
 
 const router = express.Router()
 
-router.post('/signin', signin)
+router.get('/signin', signin)
 router.post('/signout', signout)
 router.post('/revoke', revokeDiscordToken)
 
