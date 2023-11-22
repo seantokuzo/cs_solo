61c066dc4028c592fce71f36f7ddb899b8f201d0
seantokuzo <seantokuzo@gmail.com>
2023-11-21 19:34:02 -0800

    hash out user routes

--- server/routes/user-router.ts
+++ server/routes/user-router.ts
@@ -1,9 +1,18 @@
 import express from 'express'
-import { deleteUser, updateUser } from '../controllers/user-controller'
+import {
+  deleteUser,
+  getCurrentUser,
+  updateUser,
+} from '../controllers/user-controller'
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
+router.get('/current-user', currentUser, getCurrentUser)
+
+// TODO
 router.patch('/update', updateUser)
+// TODO
 router.post('/delete', deleteUser)
 
 export default router
