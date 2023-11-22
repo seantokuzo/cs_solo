f2169956db5a615bf1ca7f2edb86f15405542c0e
seantokuzo <seantokuzo@gmail.com>
2023-11-21 19:37:23 -0800

    get current user controller

--- server/controllers/user-controller.ts
+++ server/controllers/user-controller.ts
@@ -1,4 +1,17 @@
 import { Request, Response } from 'express'
+import { CurrentUserReq } from '../middlewares/current-user'
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+
+export const getCurrentUser = async (req: CurrentUserReq, res: Response) => {
+  console.log('💥 Get Current User')
+  const { currentUser } = req
+  if (!currentUser) {
+    return res.status(200).send({})
+  }
+
+  res.status(200).json(currentUser)
+}
 
 export const updateUser = async (req: Request, res: Response) => {
   console.log('💥 User Controller - Update User')
