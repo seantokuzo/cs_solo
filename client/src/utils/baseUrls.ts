402d95dfd6e8e11061261b06a04a92b54e92d720
seantokuzo <seantokuzo@gmail.com>
2023-11-21 19:36:20 -0800

    fix axios baseUrl

--- client/src/utils/baseUrls.ts
+++ client/src/utils/baseUrls.ts
@@ -1,3 +1,3 @@
-export const axiosBaseUrl = 'http://localhost:5000'
+export const axiosBaseUrl = 'http://localhost:5000/api/v1'
 export const discordLoginUrl =
   'https://discord.com/api/oauth2/authorize?client_id=1176372934177194015&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fv1%2Fauth%2Fsignin&response_type=code&scope=identify%20guilds'
