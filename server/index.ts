51e04a0d074f0f2eb957d1cfc03ac1bbf3a5a654
seantokuzo <seantokuzo@gmail.com>
2023-11-21 19:31:58 -0800

    add checks for env vars

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000
 
 const start = async () => {
   if (!process.env.MONGO_URI) throw new Error('💥 MONGO_URI must be defined')
+  if (!process.env.JWT_KEY) throw new Error('💥 JWT_KEY must be defined')
+  if (!process.env.JWT_LIFETIME)
+    throw new Error('💥 JWT_LIFETIME must be defined')
+  if (!process.env.DISCORD_OAUTH_CLIENT)
+    throw new Error('💥 DISCORD_OAUTH_CLIENT must be defined')
+  if (!process.env.DISCORD_OAUTH_SECRET)
+    throw new Error('💥 DISCORD_OAUTH_SECRET must be defined')
 
   try {
     await mongoose.connect(process.env.MONGO_URI)
